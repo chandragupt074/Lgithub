51893077f3215b119c1a7581c588cc6f7027fd2c
Chandrgupt <chandraguptsaini2@gamil.com>
2026-03-19 07:12:00 +0530

    diff-neighbour

---
+++ cpp/string/neighbour.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include <string>
+using namespace std;
+int main(){
+
+    string str = "ab";
+    int count =0;
+
+    for(int i=0;i<str.length();i++){
+        if(i==0){
+            if(str[i]!=str[i+1]) count++;
+        }
+        else if(i==str.length()-1){
+            if(str[i]!=str[i-1]) count++;
+        }
+        else{
+            if(str[i]!=str[i+1] && str[i]!=str[i-1]) count++;
+        
+        }
+        
+
+    }
+cout<<count;
+
+}
